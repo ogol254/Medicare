e26c4835e60b5c2614004f9695d0121b4e9ea9d5
masterabram <abramogol@gmail.com>
2019-03-02 11:02:45 +0300

    REFACTORING THE TESTS

---
+++ app/tests/base_tests.py
@@ -0,0 +1,136 @@
+"""
+This module tests the authentication endpoint
+Authored by: ogol
+"""
+import unittest
+import json
+import string
+from contextlib import closing
+from random import choice, randint
+
+# local imports
+from .. import create_app
+from ..db_config import destroy_db, init_db
+
+
+class BaseTest(unittest.TestCase):
+    """docstring for BaseTest"""
+    api_prefix = "/api/v1/"
+
+    def setUp(self):
+        """Performs variable definition and app initialization"""
+        self.app = create_app('testing')
+        self.client = self.app.test_client()
+
+        self.user_admin = {
+            "id_number": 33133243,
+            "first_name": "Abraham",
+            "last_name": "Ogol",
+            "address": "Nairobi",
+            "tell": "0790463533",
+            "role": "admin",
+            "password": "ogolpass"
+        }
+
+        self.user_clinician = {
+            "id_number": 33133243,
+            "first_name": "Abraham",
+            "last_name": "Ogol",
+            "address": "Nairobi",
+            "tell": "0790463533",
+            "role": "clinician",
+            "password": "ogolpass"
+        }
+
+        self.user_normal = {
+            "id_number": 33133243,
+            "first_name": "Abraham",
+            "last_name": "Ogol",
+            "address": "Nairobi",
+            "tell": "0790463533",
+            "role": "Normal",
+            "password": "ogolpass"
+        }
+
+        self.error_msg = "The path accessed / resource requested cannot be found, please check"
+
+        with self.app.app_context():
+            self.db = init_db()
+
+    def endpoint_path(self, path):
+        return "/api/v1" + path
+
+    def post(self, path, data, auth):
+        """ Make API calls for the POST method"""
+        paths = self.endpoint_path(path=path)
+        dto = json.dumps(data)
+        if auth is None:
+            headers = None
+        else:
+            headers = self.get_headers(authtoken=auth)
+        res = self.client.post(path=path, data=dto, headers=headers, content_type='application/json')
+        return res
+
+    def get(self, path, auth):
+        """ Make API calls for the POST method"""
+        paths = self.endpoint_path(path=path)
+        if auth is None:
+            headers = None
+        else:
+            headers = self.get_headers(authtoken=auth)
+        res = self.client.post(path=path, headers=headers, content_type='application/json')
+        return res
+
+    def put(self, path, data, auth):
+        """ Make API calls for the POST method"""
+        paths = self.endpoint_path(path=path)
+        dto = json.dumps(data)
+        if auth is None:
+            headers = None
+        else:
+            headers = self.get_headers(authtoken=auth)
+        res = self.client.post(path=path, data=dto, headers=headers, content_type='application/json')
+        return res
+
+    def delete(self, path, auth):
+        """ Make API calls for the POST method"""
+        paths = self.endpoint_path(path=path)
+        if auth is None:
+            headers = None
+        else:
+            headers = self.get_headers(authtoken=auth)
+        res = self.client.post(path=path, headers=headers, content_type='application/json')
+        return res
+
+    def post_user(self, role=""):
+        if role == "admin":
+            res = self.post(path="/api/v1/users/32361391", data=self.user_admin, auth=None)
+            return res
+        elif role == "Normal":
+            res = self.post(path="/api/v1/users/32361391", data=self.user_normal, auth=None)
+            return res
+        if role == "clinician":
+            res = self.post(path="/api/v1/users/32361391", data=self.user_clinician, auth=None)
+            return res
+
+    def admin_login(self):
+        register = self.post_user(role="admin")
+        login = self.post(path="/api/v1/auth/signin", data=self.user_admin, auth=None)
+        return login
+
+    def normal_login(self):
+        register = self.post_user(role="Normal")
+        login = self.post(path="/api/v1/auth/signin", data=self.user_normal, auth=None)
+        return login
+
+    def clinician_login(self):
+        register = self.post_user(role="clinician")
+        login = self.post(path="/api/v1/auth/signin", data=self.user_clinician, auth=None)
+        return login
+
+    def get_headers(self, authtoken=None):
+        headers = {
+            "Authorization": "Bearer {}".format(authtoken),
+            "content_type": 'application/json'
+        }
+        return headers
--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -12,75 +12,29 @@ from random import choice, randint
 from .. import create_app
 from ..db_config import destroy_db, init_db
 
+from .base_tests import BaseTest
 
-class TestAuth(unittest.TestCase):
+
+class TestAuth(BaseTest):
     """This class collects all the test cases for the users"""
-
-    def setUp(self):
-        """Performs variable definition and app initialization"""
-        self.app = create_app('testing')
-        self.client = self.app.test_client()
-
-        self.user = {
-            "id_number": 33133243,
-            "first_name": "Abraham",
-            "last_name": "Ogol",
-            "address": "Nairobi",
-            "tell": "0790463533",
-            "role": "Normal",
-            "password": "ogolpass"
-        }
-
-        self.error_msg = "The path accessed / resource requested cannot be found, please check"
-
-        with self.app.app_context():
-            self.db = init_db()
-
-    def post_user(self, path='', data={}):
-        if not data:
-            data = self.user
-        if not path:
-            path = '/api/v1/users/32361391'
-        # attempt to sign up
-        res = self.client.post(path=path, data=json.dumps(data), content_type='application/json')
-        return res
-
-    def login(self, path="", data={}):
-        register = self.post_user()
-        if not data:
-            data = self.user
-        if not path:
-            path = '/api/v1/auth/signin'
-        # attempt to log in
-        res = self.client.post(path=path, data=json.dumps(data), content_type='application/json')
-        return res
 
     def test_user_signup(self):
         """Test that a user can signup using a POST request"""
-        reg = self.post_user()
+        reg = self.post_user(role="admin")
         self.assertEqual(reg.json['message'], 'Successfully added')
         self.assertEqual(reg.status_code, 201)
 
     def test_user_login(self):
         """Test that a user can login using a POST request"""
-        data = {
-            "id_number": self.user['id_number'],
-            "password": self.user['password']
-        }
-        path = '/api/v1/auth/signin'
-        login = self.login(path=path, data=data)
+        login = self.admin_login()
         self.assertEqual(login.json['message'], "Success")
         #self.assertEqual(login.status_code, 200)
 
     def test_user_logout(self):
         """Test that the user can logout using a POST request"""
-        path = "/api/v1/auth/signout"
-        login = self.login()
+        login = self.admin_login()
         token = login.json['AuthToken']
-        headers = {"Authorization": "Bearer {}".format(token)}
-        logout = self.client.post(path=path,
-                                  headers=headers,
-                                  content_type="application/json")
+        logout = self.post(path="/api/v1/auth/signout", data=self.user_admin, auth=token)
         self.assertEqual(logout.status_code, 200)
 
     def test_invalid_data(self):
@@ -93,20 +47,19 @@ class TestAuth(unittest.TestCase):
                 string.ascii_letters) for x in range(randint(7, 10))),
         }
         # attempt to log in
-        path = '/api/v1/auth/signin'
-        login = self.login(path=path, data=un_user)
+        login = self.post(path="/api/v1/auth/signin", data=un_user, auth=None)
         self.assertEqual(login.status_code, 400)
 
     def test_an_unregistered_user(self):
-        """Test that an unregistered user cannot log in"""
-        data = {
-            "id_number": 32781319,
-            "password": "badadmnsn"
-        }
-        path = '/api/v1/auth/signin'
-        login = self.login(path=path, data=data)
-        self.assertEqual(login.status_code, 401)
-        self.assertEqual(login.json['message'], "Your details were not found, please sign up")
+            """Test that an unregistered user cannot log in"""
+            data = {
+                "id_number": 32781319,
+                "password": "badadmnsn"
+            }
+            path = '/api/v1/auth/signin'
+            login = self.post(path="/api/v1/auth/signin", data=data, auth=None)
+            self.assertEqual(login.status_code, 401)
+            self.assertEqual(login.json['message'], "Your details were not found, please sign up")
 
     def tearDown(self):
         """This function destroys objests created during the test run"""
